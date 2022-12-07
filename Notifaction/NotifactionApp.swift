63860792fef03808afa197c1d8e75889bd44b97e
HajarNashi <HajarNashi@gmail.com>
2022-12-07 14:45:27 +0300

    Initial Commit

---
+++ Notifaction/NotifactionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifactionApp.swift
+//  Notifaction
+//
+//  Created by Hajar Nashi on 13/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NotifactionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
